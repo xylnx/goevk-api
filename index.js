45f8f7570776fa3429bcd55b3d1f62262166edb3
xylnx <xylynx@mailbox.org>
2021-12-26 21:36:50 +0100

    Add cors

--- index.js
+++ index.js
@@ -6,12 +6,29 @@ const PORT = process.env.PORT || 5000;
 const JSON_PATH = `${__dirname}/data/events.json`;
 
 const server = http.createServer((req, res) => {
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+    'Access-Control-Max-Age': 2592000, // 30 days
+    'Content-type': 'application/json',
+    /** add other headers as per requirement */
+  };
+
   const JSON = fs.readFile(JSON_PATH, 'utf-8', (err, data) => {
     if (err)
       return console.log('AN ERROR OCCURRED reading "events.json":', err);
 
-    res.writeHead(200, { 'Content-type': 'application/json' });
-    res.end(data);
+    if (req.method === 'OPTIONS') {
+      res.writeHead(204, headers);
+      res.end();
+      return;
+    }
+
+    if (['GET', 'POST'].indexOf(req.method) > -1) {
+      res.writeHead(200, headers);
+      res.end(data);
+      return;
+    }
   });
 });
 
