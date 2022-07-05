356002d7d3c253d9d4f18046e72df15aa0b640f4
xylnx <xylnx@mailbox.org>
2022-07-05 20:10:50 +0200

    Check date validity, if not return epoch

--- scrapers/utils/createDate.js
+++ scrapers/utils/createDate.js
@@ -1,5 +1,12 @@
+function dateIsValid(date) {
+  return !isNaN(date);
+}
+
 const createDate = (year, month, day, hour, minute) => {
-  return new Date(year, month, day, hour, minute);
+  let date;
+  date = new Date(year, month, day, hour, minute);
+  if (!dateIsValid(date)) return new Date(0);
+  return date;
 };
 
 module.exports = {
